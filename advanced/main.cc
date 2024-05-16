282d99bfd00fabb698075e5a11b0ff9feec5328f
ayamir <lgt986452565@gmail.com>
2024-05-16 09:43:44 +0800

    add module example.

---
+++ advanced/main.cc
@@ -0,0 +1,7 @@
+import <iostream>;
+import mathlib;
+
+int main() {
+  std::cout << "2 + 3 = " << add(2, 3) << "\n";
+  return 0;
+}
---
+++ advanced/mathlib.cc
@@ -0,0 +1,3 @@
+export module mathlib;
+
+export int add(int a, int b) { return a + b; }
